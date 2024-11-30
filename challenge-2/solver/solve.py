400a47a97ee5de6f50c3e47d6bfd088332c589f5
annieliu2014 <157355536+annieliu2014@users.noreply.github.com>
2024-11-29 23:24:54 -0500

    Created solve.py

---
+++ challenge-2/solver/solve.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import argparse
+import socket
+
+parser = argparse.ArgumentParser(description="solve script for 'Riddle Me This'")
+parser.add_argument("--host", default="challenge", help="the host for the instance")
+parser.add_argument("--port", type=int, default=5000, help="the port of the instance")
+parser.add_argument(
+    "--print",
+    action="store_true",
+    help="print flag to stdout rather than saving to file",
+)
+args = parser.parse_args()
+
+
+def recv_until(sock, pattern):
+    response = sock.recv(4096).decode()
+    while len(response) < len(pattern) or response[-len(pattern) :] != pattern:
+        response += sock.recv(4096).decode()
+    return response
+
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.connect((args.host, args.port))
+
+# Receive welcome, riddle, and prompt
+response = recv_until(c, ":\r\n")
+
+# Extract the encrypted riddle
+split_response = response.split("\n")
+riddle = split_response[1]
+
+# Decrypt the riddle to get the key (we can use the plaintext,
+# which we already know)
+first_bytes = bytes.fromhex(riddle[:24])
+print(riddle[:24])
+plaintext = b"Never gonna "
+key_bytes = bytes(a ^ b for a, b in zip(first_bytes, plaintext))
+key = key_bytes.hex()
+
+# Send the solution to the solver
+c.sendall((key + "\n").encode())
+
+# Receive flag
+flag = recv_until(c, "\r\n")
+
+if args.print:
+    print(f"flag: {flag}")
+else:
+    with open("flag", "w") as f:
+        f.write(flag)
